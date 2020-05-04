8d70c37474250bf3e4b630552b3f53a9eb480c57
Rohit Joshi <36742838+joshirohit1988@users.noreply.github.com>
2020-05-04 14:08:21 +0530

    Fixed error while running on stackblitz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
+import { PageNotFoundComponent } from './page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
